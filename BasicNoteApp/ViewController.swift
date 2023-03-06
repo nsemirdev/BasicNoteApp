7d3b12d0c4ec904b7d99121d23c4247123049fa4
nsemirdev <nsemirdev@gmail.com>
2023-03-06 17:14:11 +0300

    Initial Commit

---
+++ BasicNoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicNoteApp
+//
+//  Created by Emir Alkal on 6.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
